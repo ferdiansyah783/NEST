762d868a6862fb74f645d9a616b9f1c7d6953874
ferdiansyah783 <mtskrmmarzuki67@gmail.com>
2022-06-12 01:08:54 +0700

    modify controller

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,9 +1,58 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('dashboard')
 export class DashboardController {
+    public data = [
+        {
+            id: 1,
+            nama: 'ferdi'
+        },
+        {
+            id: 2,
+            nama: 'anang'
+        },
+        {
+            id: 3,
+            nama: 'havis'
+        }
+    ]
+
     @Get()
     index() {
         return "Dashboard Page";
     }
+
+    @Get('contact')
+    contact(filter:string) {
+        if(filter == undefined) {
+            return "All contact";
+        }
+
+        return "filter contact by : " + filter;
+    }
+
+    @Get('filter')
+    filterContact() {
+        return this.contact('ferdi');
+    }
+
+    @Get('user')
+    user() {
+        return this.data;
+    }
+
+    @Get('user/:id')
+    otherUser(@Param() param){
+        const dataUser = this.data.filter(val => val['id'] == param.id);
+        
+        if(!dataUser.length) {
+            return "Not found";
+        }
+        
+        return dataUser;
+    }
+    
 }
+
+console.log('tester');
+
