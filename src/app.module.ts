e6ca00a29b07804d602f06122c77f52c8b523c79
ferdiansyah783 <mtskrmmarzuki67@gmail.com>
2022-06-11 17:12:45 +0700

    menmabah controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DashboardController } from './dashboard/dashboard.controller';
+import { AboutController } from './about/about.controller';
+import { ContactController } from './contact/contact.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [DashboardController, AboutController, ContactController],
 })
 export class AppModule {}
